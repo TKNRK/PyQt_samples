65fce483c6bbd341cc6e40bc675d860f2e5ebea3
takano <jugri-7441@outlook.jp>
2016-10-03 10:49:28 +0900

    up

--- pane_sample1.py
+++ pane_sample1.py
@@ -25,11 +25,8 @@ class agiWindow(QGraphicsView):
 
         self.setUp()
 
-    def initUI(self):
-        self.update()
-
     def setUp(self, ef = -1):
-        self.m_scene.setSceneRect(0, 0, 800, 800)
+        self.m_scene.setSceneRect(0, 0, self.width, self.width)
         self.counter = 0
 
         for n in self.node:
@@ -61,22 +58,65 @@ class agiWindow(QGraphicsView):
         self.setUp(self.selected)
         super(agiWindow, self).mouseMoveEvent(event)
 
+class ParamView(QGraphicsView):
+    def __init__(self, width=100,height=50,size=5):
+        super(ParamView, self).__init__()
+
+        self.p_scene = QGraphicsScene()
+        self.setScene(self.p_scene)
+
+        # パラメータを作るクラスを生成する
+        #agi = AGI(width, width-100, 2)
+        self.width = width
+        self.height = height
+        self.size = size
+        self.vals = np.array([1.2,0.3,4.0,2.8])
+        self.bars = np.array([0,1,2,3,4])
+        
+        self.lines = []
+        self.rects = []
+        
+        self.setUp()
+
+    def setUp(self):
+        self.p_scene.setSceneRect(0, 0, 100, 50)
+        self.counter = 0
+        div = len(self.bars) - 1
+        length = self.width / div
+        # min > 0 の場合のみ考えている
+        vals_fixed = self.vals / max(self.vals) * 40
+
+        for i in range(div):
+            pos_x = 10 + length*(i-1)
+            pos_y = 5 + (40 - vals_fixed[i])
+            item = QGraphicsRectItem(pos_x,pos_y,length,vals_fixed[i])
+
+            item.setPen(QPen(Qt.black, 1))
+            item.setBrush(QBrush(Qt.white))
+
+            self.p_scene.addItem(item)
+            self.rects.append(item)
+
+
 class MainWindow(QWidget):
     def __init__(self,parent=None):
         super(MainWindow, self).__init__(parent)
 
         # グラフ描画領域の埋め込み
-        self.graphDrawer = agiWindow(800, 800)
-        self.graphicsView = self.graphDrawer
-
+        self.graphicsView = agiWindow(800, 800)
+        
         # パラメータ領域の埋め込み
         self.nullButton1 = QPushButton("&NULL1")
         self.nullButton1.clicked.connect(self.do_nothing)
+        self.paramView1 = ParamView()
         self.nullButton2 = QPushButton("&NULL2")
         self.nullButton2.clicked.connect(self.do_nothing)
+        self.paramView2 = ParamView()
         buttonLayout = QVBoxLayout()
         buttonLayout.addWidget(self.nullButton1)
+        buttonLayout.addWidget(self.paramView1)
         buttonLayout.addWidget(self.nullButton2)
+        buttonLayout.addWidget(self.paramView2)
 
         # 文字列出力領域の埋め込み
         #textLayout = QHBoxLayout
